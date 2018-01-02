9f14890f5b8168bc3bcba4f71676707ab5af0844
haikmanukyan <haik.manukyan@gmail.com>
2018-01-02 19:11:17 +0400

    Dummy Assets

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour 
+{
+	public float Torque = 1.0f;
+	private Rigidbody2D rigidbody;
+
+	void Start () 
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () 
+	{	
+		rigidbody.MoveRotation(rigidbody.rotation + Torque);
+	}
+}
